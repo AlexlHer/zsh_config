adc656f5dbbd99d136deaca268644ded3795233b
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2023-02-16 09:29:11 +0100

    Edit PATH

--- _local_c.zsh
+++ _local_c.zsh
@@ -8,8 +8,9 @@
 # ---------------------------------------------------------------
 
 export PATH="\
-/usr/local/cmake-3.24.1/bin\
-:${HOME}/local_prog/doxygen-1.9.3/bin\
+${HOME}/.local/bin\
+:/usr/local/cmake-3.24.1/bin\
+:${HOME}/local_prog/doxygen-1.9.6/bin\
 :${HOME}/local_prog/node-v18.9.0-linux-x64/bin\
 :${HOME}/.arm/forge/22.1.2/bin\
 :${PATH}"
