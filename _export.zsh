0c057ea60f3313aef61ee68cbf1c83d7d550e337
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2023-08-26 17:21:23 +0200

    Add specific build_install directory for containers -- Update podman function

--- _export.zsh
+++ _export.zsh
@@ -9,8 +9,11 @@
 
 export WORK_DIR=${HOME}/work
 export BUILD_DIR=${HOME}/build_install
+export CONTAINER_BUILD_DIR=${BUILD_DIR}/container
 
 export CCACHE_COMPRESS=true
 export CCACHE_COMPRESSLEVEL=6
 export CCACHE_DIR=${BUILD_DIR}/ccache
 export CCACHE_MAXSIZE=20G
+
+mkdir -p ${CCACHE_DIR} ${WORK_DIR} ${BUILD_DIR} ${CONTAINER_BUILD_DIR}
--- _podman.zsh
+++ _podman.zsh
@@ -18,28 +18,29 @@ initpm()
   IMAGE=$1
 
   _pzc_info "Creating and running container..."
-  CONTAINER=$(pm run -v /home/alexandre/work:/root/work:ro -e OMPI_ALLOW_RUN_AS_ROOT=1 -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 -dt $IMAGE)
+  CONTAINER=$(pm run \
+    -v ${WORK_DIR}:/root/work:ro \
+    -v ${CONTAINER_BUILD_DIR}:/root/build_install \
+    -e OMPI_ALLOW_RUN_AS_ROOT=1 \
+    -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 \
+    -e USER=root \
+    -w /root \
+    -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -dt $IMAGE \
+  )
   export CONTAINER=$(echo $CONTAINER | tail -n 1)
 
-  _pzc_info "Executing command 'apt-get update' in container..."
+  _pzc_info "Installing additionnal packages in container..."
   pm exec $CONTAINER apt-get update
+  pm exec $CONTAINER apt-get install zsh unzip exa git mold vim gdb xcb libgtk-3-common libasound2 libdbus-glib-1-2 -y
 
-  _pzc_info "Executing command 'apt-get install zsh unzip exa git mold vim -y' in container..."
-  pm exec $CONTAINER apt-get install zsh unzip exa git mold vim -y
-
-  _pzc_info "Executing command 'curl -o /root/install.sh https://ohmyposh.dev/install.sh' in container..."
+  _pzc_info "Installing OhMyPosh in container..."
   pm exec $CONTAINER curl -o /root/install.sh https://ohmyposh.dev/install.sh
-
-  _pzc_info "Executing command 'chmod u+x /root/install.sh' in container..."
   pm exec $CONTAINER chmod u+x /root/install.sh
-
-  _pzc_info "Executing command 'bash -c /root/install.sh' in container..."
   pm exec $CONTAINER bash -c /root/install.sh
 
-  _pzc_info "Executing command 'git clone https://github.com/AlexlHer/zsh_config /root/.pzc' in container..."
+  _pzc_info "Installing PZC in container..."
   pm exec $CONTAINER git clone https://github.com/AlexlHer/zsh_config /root/.pzc
-
-  _pzc_info "Executing command 'cp /root/.pzc/home.zshrc /root/.zshrc' in container..."
   pm exec $CONTAINER cp /root/.pzc/home.zshrc /root/.zshrc
 
   _pzc_info "Copying .zhistory in container..."
@@ -47,5 +48,5 @@ initpm()
 
   echo ""
   _pzc_info "Executing container..."
-  _pzc_coal_eval "pm exec -w /root -it $CONTAINER zsh"
+  _pzc_coal_eval "pm exec -it $CONTAINER zsh"
 }
