d1ad05e59c694e04a93c391f2be33e7443e64588
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2024-07-04 08:38:01 +0200

    Change chmod default value for containers

--- _podman.zsh
+++ _podman.zsh
@@ -95,6 +95,7 @@ then
     pm exec $CONTAINER git clone https://github.com/AlexlHer/zsh_config /root/.pzc
     pm exec $CONTAINER cp /root/.pzc/home.zshrc /root/.zshrc
     pm exec $CONTAINER sed -i 's/local _PZC_AGE_AVAILABLE=1/local _PZC_AGE_AVAILABLE=0/g' /root/.zshrc
+    pm exec $CONTAINER sed -i 's/local _PZC_CHMOD_COMPILING=0/local _PZC_CHMOD_COMPILING=1/g' /root/.zshrc
 
     _pzc_info "Copying .zhistory in container..."
     pm cp $HOME/.zhistory $CONTAINER:/root/.zhistory
